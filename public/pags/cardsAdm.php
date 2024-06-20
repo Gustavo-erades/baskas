eb6099f76749007cde2f5e6412bbba77175dce82
Gustavo-erades <eradesvilarinho@gmail.com>
2024-06-20 00:30:41 -0300

    iniciando layout do painel administrativo

---
+++ public/pags/cardsAdm.php
@@ -0,0 +1,82 @@
+<div class="container">
+  <div id="procuraJogador">
+    <h2>Jogadores</h2>
+    <hr>
+    <div class="divPesquisa">
+      <label for="inputPesquisa">Procurar Jogador</label>
+      <input type="text" class="form-control" id="inputPesquisa" placeholder="Nome, pontuação, etc...">
+    </div>
+  </div>
+  <div class="cards_jogadores">
+    <div class="card">
+      <div class="row">
+        <div class="col-md-4">
+          <img src="../assets/images/imgTeste.jpeg" class="card-image-top image-fluid rounded-start" width="100%"
+            height="100%" id="imagem-tela-grande">
+          <img src="../assets/images/imgTeste.jpeg" class="card-image-top image-fluid rounded-top" width="100%"
+            height="100%" id="imagem-tela-pequena">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title nomeJogador">Nome</h5>
+            <ul class="ulAttr">
+               <li class="liAttr">
+                <h6>Passe</h6>
+                ??
+              </li>
+              <li class="liAttr">
+                <h6>Shot</h6>
+                ??
+              </li>
+              <li class="liAttr">
+                <h6>Shot 3</h6>
+                 ??
+              </li>
+              <li class="liAttr">
+                <h6>Infilt</h6>
+                ??
+              </li>
+              <li class="liAttr">
+                <h6>Bandj</h6>
+                ??
+              </li>
+              <li class="liAttr">
+                <h6>Contrl</h6>
+                ??
+              </li>
+              <li class="liAttr">
+                 <h6>Resist</h6>
+                ??
+              </li>
+              <li class="liAttr">
+                <h6>Roubo</h6>
+                ??
+              </li>
+              <li class="liAttr">
+                <h6>Força</h6>
+                ??
+              </li>
+              <li class="liAttr">
+                <h6>Defesa</h6>
+                ??
+              </li>
+            </ul>
+            <hr>
+            <div class="row">
+              <ul class="col-12 col-md-6 ulAttr attrFinais">
+                <li>Visão ?? </li>
+                <li>Clutch ??</li>
+                <li>Decisão ??</li>
+              </ul>
+              <div class="col-12 col-md-6 divEloCard">
+                <span class="eloCard">
+                  <h4 class="numElo" style="color: #3e0d01;">90</h4>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
