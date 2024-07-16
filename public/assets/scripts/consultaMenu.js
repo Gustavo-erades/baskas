3119a477c9786e65a978b796330b7e5734bb11cd
Gustavo-erades <eradesvilarinho@gmail.com>
2024-07-16 12:38:27 -0300

    ajustes no js da navbar

--- public/assets/scripts/consultaMenu.js
+++ public/assets/scripts/consultaMenu.js
@@ -4,10 +4,10 @@ let codValue = params.get('cod');
 var navCards=document.querySelector("#navCards");
 var navTitulos=document.querySelector("#navTitulos");
 if(codValue.startsWith('adm-0')){
-    navCards.classList.toggle("active");
-    navTitulos.classList.toggle("active");
+    navCards.classList.add("active");
+    navTitulos.classList.remove("active");
 }else if(codValue.startsWith('adm-1')){
-    navCards.classList.toggle("active");
-    navTitulos.classList.toggle("active");
+    navCards.classList.remove("active");
+    navTitulos.classList.add("active");
 
 }
