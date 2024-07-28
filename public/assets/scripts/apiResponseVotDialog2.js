1bcdb1800715b8132324d3525f27206625c88d49
Gustavo-erades <eradesvilarinho@gmail.com>
2024-07-28 17:40:01 -0300

    modal nos votos

---
+++ public/assets/scripts/apiResponseVotDialog2.js
@@ -0,0 +1,5 @@
+function apiResponseVotDialog(elemento){
+    const idDoBotao = elemento.id;
+    const modal=document.querySelector("#dialogVot"+idDoBotao+"")
+    modal.showModal();
+}
--- public/pags/componentes/adm/resultVotacao/votacao.php
+++ public/pags/componentes/adm/resultVotacao/votacao.php
@@ -1,7 +1,6 @@
 <a class="btn" id="lancarVotacao">
     Lançar votação
 </a>
-<script src="./assets/scripts/apiResponseVot.js"></script>
 <div>
     <h3 id="contagemVotos">
         <span id="numVotos"></span> votos
@@ -12,7 +11,7 @@
             <h3>MVP</h3>
         </span>
         <h4>mais votado (<span id="quantVotMvp"></span>) : <span id="nomeVotMvp">nome</span></h4>
-        <button class="btn btn-link">mais votos</button>
+        <button class="btn btn-link" id="Mvp" onclick="apiResponseVotDialog(this)">mais votos</button>
     </div>
     <!-- mip -->
     <div class="divVotTitulo">
@@ -20,7 +19,7 @@
             <h3>MIP</h3>
         </span>
         <h4>mais votado (<span id="quantVotMip"></span>) : <span id="nomeVotMip">nome</span></h4>
-        <button class="btn btn-link">mais votos</button>
+        <button class="btn btn-link" id="Mip" onclick="apiResponseVotDialog(this)">mais votos</button>
     </div>
     <!-- Bagre -->
     <div class="divVotTitulo">
@@ -28,25 +27,60 @@
             <h3>Bagre</h3>
         </span>
         <h4>mais votado (<span id="quantVotBagre"></span>) : <span id="nomeVotBagre">nome</span></h4>
-        <button class="btn btn-link">mais votos</button>
+        <button class="btn btn-link" id="Bagre" onclick="apiResponseVotDialog(this)">mais votos</button>
     </div>
 </div>
-<!-- modal de mais votos 
+<!-- modal de mais votos -->
 <div id="divDialog">
-    <dialog id="responseCard<?= $card['nome'] ?>" class="bg-dark">
+    <dialog id="dialogVotMvp" class="bg-dark"> 
         <table class="table table-dark table-hover">
             <thead>
-                <th>Histórico</th>
-                <th>Data</th>
+                <th>Nome</th>
+                <th>Quant. votos</th>
             </thead>
-            <tbody id="response<?= $card['nome'] ?>">
+            <tbody id="responseVotMvp">
             </tbody>
         </table>
         <div>
-            <button type="button" class="btn btn-link" data-target="responseCard<?= $card['nome'] ?>" id="fecharModal">
+            <button type="button" class="btn btn-link" data-target="dialogVotMvp" id="fecharModal" data-target="dialogVotMvp">
+                <a>fechar</a>
+            </button>
+        </div>
+    </dialog>
+
+    <dialog id="dialogVotMip" class="bg-dark">
+        <table class="table table-dark table-hover">
+            <thead>
+                <th>Nome</th>
+                <th>Quant. votos</th>
+            </thead>
+            <tbody id="responseVotMip">
+            </tbody>
+        </table>
+        <div>
+            <button type="button" class="btn btn-link" data-target="dialogVotMip" id="fecharModal" data-target="dialogVotMip">
+                <a>fechar</a>
+            </button>
+        </div>
+    </dialog>
+
+    <dialog id="dialogVotBagre" class="bg-dark">
+        <table class="table table-dark table-hover">
+            <thead>
+                <th>Nome</th>
+                <th>Quant. votos</th>
+            </thead>
+            <tbody id="responseVotBagre">
+            </tbody>
+        </table>
+        <div>
+            <button type="button" class="btn btn-link" data-target="dialogVotBagre" id="fecharModal" data-target="dialogVotBagre">
                 <a>fechar</a>
             </button>
         </div>
     </dialog>
 </div>
--->
+<link rel="stylesheet" href="./assets/styles/styleDialog.css">
+<script src="./assets/scripts/apiResponseVot.js"></script>
+<script src="./assets/scripts/apiResponseVotDialog2.js"></script>
+<script src="./assets/scripts/fecharModal.js"></script>
