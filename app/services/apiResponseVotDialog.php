8c5e7083200ab952ab6b7930c8762e1259172c06
Gustavo-erades <eradesvilarinho@gmail.com>
2024-07-28 18:36:51 -0300

    segunda api de votacoes

---
+++ app/services/apiResponseVotDialog.php
@@ -0,0 +1,32 @@
+<?php
+require_once("../database/conexao.php");
+header('Content-Type: application/json');
+$return = array();
+//$votTitulo=strtolower($_POST['votTitulo']);
+//echo $votTitulo;
+$sql = '
+        SELECT mvp AS nomeVotMvp, COUNT(*) AS quantVotMvp
+        FROM bd_baskas.votacao
+        GROUP BY mvp
+        ORDER BY quantVotMvp DESC; 
+    ';
+$stmt = mysqli_prepare($conn, $sql);
+if ($stmt === false) {
+    die('Erro na preparação da declaração SQL (apiResponseVotDialog)' . mysqli_error($conn));
+}
+//mysqli_stmt_bind_param($stmt, 'ss', $votTitulo,$votTitulo);
+$result = mysqli_stmt_execute($stmt);
+if ($result === false) {
+    die('Erro na excução da query SQL (apiResponseVotDialog):' . mysqli_error($conn));
+} else {
+    $resultSql = mysqli_stmt_get_result($stmt);
+    if (mysqli_num_rows($resultSql) > 0) {
+        while ($row = mysqli_fetch_assoc($resultSql)) {
+            $return[] = $row;
+        }
+        echo var_dump($_POST);
+    } else {
+        $return[] = array('erro' => 'nenhum voto até o momento.');
+    }
+    echo json_encode($return);
+}
--- public/assets/scripts/apiResponseVotDialog2.js
+++ public/assets/scripts/apiResponseVotDialog2.js
@@ -1,5 +1,41 @@
 function apiResponseVotDialog(elemento){
     const idDoBotao = elemento.id;
+    console.log(idDoBotao)
     const modal=document.querySelector("#dialogVot"+idDoBotao+"")
     modal.showModal();
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '../app/services/apiResponseVotDialog.php', true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onload = function () {
+        if (xhr.status >= 200 && xhr.status < 400) {
+            var data = JSON.parse(xhr.responseText);
+            const ulResponse = document.querySelector('#responseVot'+idDoBotao);
+            ulResponse.innerHTML='';
+            mostrarDados(data,ulResponse);
+        } else {
+            console.error('Erro ao carregar dados: ' + xhr.statusText);
+        }
+    };
+    xhr.onerror = function () {
+        console.error('Erro de rede ao tentar carregar dados.');
+    };
+    const teste='votTitulo=' + encodeURIComponent(idDoBotao)
+    console.log(teste)
+    xhr.send(teste);
+}
+function mostrarDados(data,elementoPai) {
+    data.forEach(function (item) {
+        if ('erro' in item) {
+            console.log(item.erro);
+        } else {
+            const li=document.createElement('tr');
+            const td1=document.createElement('td');
+            const td2=document.createElement('td');
+            td1.textContent=item.nomeVotMvp;
+            td2.textContent=item.quantVotMvp;
+            li.appendChild(td1);
+            li.appendChild(td2);
+            elementoPai.appendChild(li);
+        }
+    });
 }
